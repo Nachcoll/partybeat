9507ef9df47b798464ddc3fad3996812147e7733
Ignacio Coll García <nachcoll@gmail.com>
2022-06-01 11:03:05 +0200

    chat in socket.io implemented

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 'use strict'
 
-// const Express = require('express')
-// const cors = require('cors')
-// const router = require('./router/router')
+import http from 'http'
 import Express from 'express'
 import cors from 'cors'
 import router from './router/router.js'
+import { Server } from 'socket.io'
 
 const PORT = 8000
 
@@ -17,7 +16,44 @@ app.use(cors())
 app.use(Express.json())
 app.use(router)
 
-app.listen(PORT, () => {
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors:{
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
+
+//io connection + send info back
+
+io.on('connection', (socket)=> {
+  console.log('user just connected to the room')
+
+//io room (joining and leaving the previous one)
+let currentRoom;
+socket.on('join_room', (data) => {
+  if(currentRoom)   socket.leave(currentRoom)
+  currentRoom = data;
+  socket.join(data)
+  console.log(socket.rooms)
+})
+
+
+
+  socket.on('send_message', (data) => {
+    console.log(data)
+    //sending to everybody =
+    // socket.broadcast.emit('receive_message', data)
+    socket.to(data.room).emit('receive_message', data)
+  })
+})
+
+
+
+
+server.listen(PORT, () => {
   console.log(`connected to http://localhost:${PORT}`)
 })
 
