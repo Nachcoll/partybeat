2a55b0b3447e00a500969ba942b5e254b15c6a5c
Ignacio Coll García <nachcoll@gmail.com>
2022-06-13 21:12:19 +0200

    test

--- client/src/Components/Dashboard/Dashboard.tsx
+++ client/src/Components/Dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
 import "./Dashboard.css";
 import logo from "../../images/logo.png";
-import frontURL from "../../utils/deployment-url";
+import rootURL from "../../utils/utils";
 
-const rootURL = frontURL
+// const rootURL = frontURL
 
 const Dashboard = () => {
   //we don't fetch here because we are actually just redirecting to Spotify Authorization site.
