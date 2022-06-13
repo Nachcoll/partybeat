bbab32bffda2b6976b201955a07cf055e87165a6
Ignacio Coll García <nachcoll@gmail.com>
2022-06-13 21:03:15 +0200

    fix: bug with urls

--- client/src/Services/socket.tsx
+++ client/src/Services/socket.tsx
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-import rootURL from "../utils/utils.js";
+import rootURL from '../utils/utils';
 
-export default io(rootURL.rootURL);
+export default io(rootURL);
